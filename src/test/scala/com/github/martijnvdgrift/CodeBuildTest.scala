a2178700a30fe3f3b332ae9f74928c1911054306
Martijn van de Grift <martijn.van.de.grift@trivento.nl>
2017-12-15 13:20:23 +0100

    initial commit

---
+++ src/test/scala/com/github/martijnvdgrift/CodeBuildTest.scala
@@ -0,0 +1,11 @@
+package com.github.martijnvdgrift
+
+import org.scalatest.{FlatSpec, Matchers}
+
+class CodeBuildTest extends FlatSpec with Matchers  {
+
+  it should "fail the build " in {
+    1 shouldEqual 2
+  }
+
+}
